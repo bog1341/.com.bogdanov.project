ee35e49cdfab329b5c4f2326cf15c0835fa79f1d
bog1341 <bog1341@gmail.com>
2019-03-30 10:27:37 +0200

    Calculate IP Address

--- src/CalcIP.java
+++ src/CalcIP.java
@@ -1,2 +1,17 @@
 public class CalcIP {
+    public static void main(String[] args){
+        int x = 64;
+
+        int i = 128;
+        while (i > 0) {
+            if (x >= i) {
+                x -= i;
+                System.out.print("1");
+            } else
+                System.out.print("0");
+            i /= 2;
+
+
+        }
+    }
 }
