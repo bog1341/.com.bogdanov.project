4da1fe766e2312daab20668e0cc35522a0b69c22
bog1341 <bog1341@gmail.com>
2019-03-30 11:36:58 +0200

    Show Bits

---
+++ src/ShowBitsDemo.java
@@ -0,0 +1,45 @@
+class ShowBits{
+    int numbits;
+
+    ShowBits(int n){
+        numbits = n;
+    }
+
+    void show(long val){
+        long mask = 1;
+        mask <<= numbits - 1;
+        int spacer = 0;
+        for (; mask != 0; mask >>>= 1){
+            if ((val & mask) != 0) System.out.println("1");
+            else System.out.println("0");
+            spacer++;
+            if ((spacer % 8) == 0){
+                System.out.println(" ");
+                spacer = 0;
+            }
+        }
+        System.out.println();
+    }
+}
+
+
+public class ShowBitsDemo {
+    public static void main(String args[]){
+        ShowBits b = new ShowBits(8);
+        ShowBits i = new ShowBits(16);
+        ShowBits li = new ShowBits(32);
+
+        System.out.print("123 в двоичном: ");
+        b.show(123);
+
+        System.out.println("\n87987 в двоичном: ");
+        i.show(87987);
+
+        System.out.println("\n237658768 в двоичном: ");
+        li.show(237658768);
+
+        System.out.println("\nМладшие биты 87987: ");
+        b.show(87987);
+
+    }
+}
